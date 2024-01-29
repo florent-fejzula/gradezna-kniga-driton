50ff0f6a8f62ad3faa29d9c5673fc4d556cf2195
Florent <dodi-messi19@hotmail.com>
2024-01-29 12:34:49 +0100

    template with input fields

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  gradbaInputValue: string = '';
+  izveduvacInputValue: string = '';
+  knigaInputValue: string = '';
+  datumInputValue: string = '';
+  investorInputValue: string = '';
+  adresaInputValue: string = '';
+  pozicijaInputValue: string = '';
+  merkaInputValue: string = '';
+  cenaInputValue: number | undefined;
+
+  printThisPage() {
+    window.print();
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NoopAnimationsModule,
+    MatInputModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
