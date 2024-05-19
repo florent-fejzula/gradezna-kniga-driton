0b76addecd91a0887231e9e74b074cda1d533a79
Florent <dodi-messi19@hotmail.com>
2024-05-19 12:17:14 +0200

    Fari new rows and formula

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,23 @@ import { ExportService } from './export.service';
 import { FileSaveDialogComponent } from './file-save-dialog/file-save-dialog.component';
 import { MatDialog } from '@angular/material/dialog';
 
+interface TableRow {
+  redenBrojArea: string;
+  textAreaInput: string;
+  kolicinaArea: string;
+  merkaArea: string;
+  cenaArea: string;
+  vkupnoArea: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  gradbaBroj: string = '';
   gradbaInputValue: string = '';
-  // izveduvacInputValue: string = '';
   knigaInputValue: string = '';
   datumInputValue: string = '';
   investorInputValue: string = '';
@@ -18,18 +27,30 @@ export class AppComponent {
   pozicijaInputValue: string = '';
   merkaInputValue: string = '';
   cenaInputValue: number | undefined;
-  redenBrojArea: string = '';
-  textAreaInput: string = '';
-  kolicinaArea: string = '';
-  merkaArea: string = '';
-  cenaArea: string = '';
-  vkupnoArea: string = '';
-  div2InputValue: string = '';
-  div3InputValue: string = '';
-  div4InputValue: string = '';
+  // redenBrojArea: string = '';
+  // textAreaInput: string = '';
+  // kolicinaArea: string = '';
+  // merkaArea: string = '';
+  // cenaArea: string = '';
+  // vkupnoArea: string = '';
   exportFileName: string = 'exported-data.json'; // Default file name
 
-  fontSize: number = 20;
+  fontSize: number = 16;
+  fontSizePozicija: number = 20;
+
+  tableData: TableRow[] = [
+    {
+      redenBrojArea: '1',
+      textAreaInput: '',
+      kolicinaArea: '',
+      merkaArea: '',
+      cenaArea: '',
+      vkupnoArea: '',
+    },
+  ];
+
+  div4InputValue: number = 0;
+  isDiv4InputDisabled: boolean = true;
 
   constructor(private exportService: ExportService, public dialog: MatDialog) {}
 
@@ -41,20 +62,25 @@ export class AppComponent {
     this.fontSize = Math.max(10, this.fontSize - 1);
   }
 
+  increasePozicijaFontSize() {
+    this.fontSizePozicija += 1;
+  }
+
+  decreasePozicijaFontSize() {
+    this.fontSizePozicija = Math.max(10, this.fontSizePozicija - 1);
+  }
+
   exportToJson(): void {
     const dataToExport = {
+      gradbaBroj: this.gradbaBroj,
       gradbaInputValue: this.gradbaInputValue,
       knigaInputValue: this.knigaInputValue,
       datumInputValue: this.datumInputValue,
       investorInputValue: this.investorInputValue,
       adresaInputValue: this.adresaInputValue,
       pozicijaInputValue: this.pozicijaInputValue,
-      redenBrojArea: this.redenBrojArea,
-      textAreaInput: this.textAreaInput,
-      kolicinaArea: this.kolicinaArea,
-      merkaArea: this.merkaArea,
-      cenaArea: this.cenaArea,
-      vkupnoArea: this.vkupnoArea,
+      tableData: this.tableData,
+      div4InputValue: this.div4InputValue
     };
 
     const dialogRef = this.dialog.open(FileSaveDialogComponent, {
@@ -84,22 +110,58 @@ export class AppComponent {
       reader.onload = (e) => {
         const importedData = JSON.parse(reader.result as string);
 
-        (this.gradbaInputValue = importedData.gradbaInputValue),
+        (this.gradbaBroj = importedData.gradbaBroj),
+          (this.gradbaInputValue = importedData.gradbaInputValue),
           (this.knigaInputValue = importedData.knigaInputValue),
           (this.datumInputValue = importedData.datumInputValue),
           (this.investorInputValue = importedData.investorInputValue),
           (this.adresaInputValue = importedData.adresaInputValue),
           (this.pozicijaInputValue = importedData.pozicijaInputValue),
-          (this.redenBrojArea = importedData.redenBrojArea);
-        this.textAreaInput = importedData.textAreaInput;
-        this.kolicinaArea = importedData.kolicinaArea;
-        this.merkaArea = importedData.merkaArea;
-        this.cenaArea = importedData.cenaArea;
-        this.vkupnoArea = importedData.vkupnoArea;
+          (this.tableData = importedData.tableData),
+          (this.div4InputValue = importedData.div4InputValue);
       };
 
       reader.readAsText(file);
     }
+  }
+
+  addNewRow() {
+    const nextRedenBroj = this.tableData.length + 1;
+    this.tableData.push({
+      redenBrojArea: nextRedenBroj.toString(),
+      textAreaInput: '',
+      kolicinaArea: '',
+      merkaArea: '',
+      cenaArea: '',
+      vkupnoArea: '',
+    });
+    this.calculateDiv4InputValue();
+  }
+
+  calculateVkupnoArea(row: TableRow) {
+    const kolicina = parseFloat(row.kolicinaArea);
+    const cena = parseFloat(row.cenaArea);
+    if (!isNaN(kolicina) && !isNaN(cena)) {
+      row.vkupnoArea = (kolicina * cena).toFixed(2);
+    } else {
+      row.vkupnoArea = '';
+    }
+    this.calculateDiv4InputValue();
+  }
+
+  calculateDiv4InputValue() {
+    const total = this.tableData.reduce((total, row) => {
+      return total + (parseFloat(row.vkupnoArea) || 0);
+    }, 0);
+
+    // Append .00 to the formatted total
+    const formattedTotal = total.toFixed(2) + '.00';
+
+    this.div4InputValue = parseFloat(formattedTotal);
+  }
+
+  removeRow(index: number) {
+    this.tableData.splice(index, 1);
   }
 
   printThisPage() {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,31 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { MatTableModule } from '@angular/material/table';
 import { FileSaveDialogComponent } from './file-save-dialog/file-save-dialog.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FileSaveDialogComponent
-  ],
+  declarations: [AppComponent, FileSaveDialogComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     NoopAnimationsModule,
     FormsModule,
     MatInputModule,
+    MatIconModule,
     MatFormFieldModule,
     MatTableModule,
-    MatDialogModule
+    MatDialogModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
